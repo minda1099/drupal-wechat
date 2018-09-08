dad5b9a4f101d58abb7bd3b1c8b82c70da2b5123
Minda Zhang <minda1099@126.com>
2018-09-08 16:05:41 +0800

    7.x_2.0 version

--- tencent_wechat.api.php
+++ tencent_wechat.api.php
@@ -3,16 +3,16 @@
 /**
  * Allow response message to be altered.
  *
- * @param \EasyWeChat\Message\AbstractMessage $response_message
+ * @param EasyWeChat\Kernel\Messages\Message $response_message
  *   The response message to be altered.
  * @param array $context
  *   Keys:
  *     - request_message, \EasyWeChat\Support\Collection.
  */
-function hook_tencent_wechat_response_alter(\EasyWeChat\Message\AbstractMessage &$response_message, array $context) {
+function hook_tencent_wechat_response_alter(EasyWeChat\Kernel\Messages\Message &$response_message, array $context) {
   $request_message = $context["request_message"];
-  if ($response_message instanceof \EasyWeChat\Message\Text) {
-    $message = "Hi, " . $request_message->FromUserName;
+  if ($response_message instanceof EasyWeChat\Kernel\Messages\Text) {
+    $message = "Hi, " . $request_message['FromUserName'];
     $message .= ", This message is altered and responsed from " . __FUNCTION__;
     $response_message->content = $message;
   }
@@ -21,12 +21,12 @@ function hook_tencent_wechat_response_alter(\EasyWeChat\Message\AbstractMessage 
 /**
  * Allow request message to be processed by extend modules.
  *
- * @param \EasyWeChat\Support\Collection $request_message
+ * @param array $request_message
  *   The request message to be handled further.
  */
-function hook_tencent_wechat_message(\EasyWeChat\Support\Collection $request_message) {
-  watchdog("tencent_wechat config",
+function hook_tencent_wechat_message($request_message) {
+  watchdog("tencent_wechat message",
     "A message from @openid",
-    ["@openid" => $request_message->FromUserName]
+    ["@openid" => $request_message['FromUserName']]
   );
 }
